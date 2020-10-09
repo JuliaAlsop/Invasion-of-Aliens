86e9b26fb524062d3ab9152a10b73f95bbb2ebab
Julia Alsop <64661590+JuliaAlsop@users.noreply.github.com>
2020-10-08 22:02:14 -0400

    Additional pictorial files...still working on

---
+++ node_modules/@amcharts/amcharts4/.internal/core/Options.js
@@ -0,0 +1,21 @@
+/**
+ * Global options.
+ */
+export var options = {
+    verbose: true,
+    commercialLicense: false,
+    classNamePrefix: "amcharts-",
+    autoSetClassName: false,
+    minPolylineStep: 0.5,
+    onlyShowOnViewport: false,
+    queue: false,
+    autoDispose: false,
+    licenses: [],
+    suppressErrors: false,
+    suppressWarnings: false,
+    animationsEnabled: true,
+    nonce: "",
+    deferredDelay: 100,
+    disableHoverOnTransform: "never"
+};
+//# sourceMappingURL=Options.js.map
